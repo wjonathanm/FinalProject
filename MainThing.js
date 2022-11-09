23534f28eadb781688a2f4d0146c83961b6a1264
jakedudziak <55498677+jakedudziak@users.noreply.github.com>
2022-11-09 11:45:38 -0600

    changed the nav

--- MainThing.js
+++ MainThing.js
@@ -18,6 +18,10 @@ app.get("/EmployeePTO/Request", (req, res) => {
     res.render('Request', {
     })
 });
+app.get("/EmployeePTO/History", (req, res) => {
+        res.render('History', {
+    })
+});
 
 let port = 3000;
 app.listen( port, ()=>{
